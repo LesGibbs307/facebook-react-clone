542d32a59f91d25bcac35e34f2103bdaf7c44b6b
John Gibson <john@iamjohniii.com>
2020-09-27 21:50:10 -0400

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBLJoJ-G7HoOvL_THmhQtzPS5jCAr49Ls",
+    authDomain: "facebook-clone-17a95.firebaseapp.com",
+    databaseURL: "https://facebook-clone-17a95.firebaseio.com",
+    projectId: "facebook-clone-17a95",
+    storageBucket: "facebook-clone-17a95.appspot.com",
+    messagingSenderId: "453287418793",
+    appId: "1:453287418793:web:79899f6f054783696082b8"
+  };
